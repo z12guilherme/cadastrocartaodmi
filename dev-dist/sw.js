b3b412c935d127fe2c5756c63c9f222e68e42fad
ti <mguimarcos39@gmail.com>
2026-03-12 13:47:14 -0300

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.82pij18n0j4"
+    "revision": "0.ffpvp02ojt"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
