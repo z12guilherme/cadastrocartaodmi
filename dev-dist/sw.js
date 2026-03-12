cd4085ce5ebc7050297038896ea74b101b7ce9fc
ti <mguimarcos39@gmail.com>
2026-03-12 14:16:08 -0300

    sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ffpvp02ojt"
+    "revision": "0.1vlpbbqmar4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
