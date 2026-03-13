22acd51772f98923f398862abb8e145592f7a066
ti <mguimarcos39@gmail.com>
2026-03-13 13:26:13 -0300

    trocando fundo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.luvolkq4lk8"
+    "revision": "0.8j3as9iohko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
