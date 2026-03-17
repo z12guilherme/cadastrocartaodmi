a2bd4dcff37ff194ea57cffa83af671ce9888814
ti <mguimarcos39@gmail.com>
2026-03-17 10:57:17 -0300

    Pré visualização

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4lgj49fg904"
+    "revision": "0.ndna88u4r4k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
