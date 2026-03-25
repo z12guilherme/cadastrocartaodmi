553ccdb1276359aac3521acb62bb5483e28c3278
ti <mguimarcos39@gmail.com>
2026-03-25 11:08:14 -0300

    Resolvendo Conflito

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  assetsInclude: ['**/*.JPG'],
   server: {
     host: "::",
     port: 8080,
